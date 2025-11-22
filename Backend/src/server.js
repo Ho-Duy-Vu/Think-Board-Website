15cf52178caa487b229ede373dad4cc54aeea4bf
Hồ Duy Vũ <duyvu11092004@gmail.com>
2025-11-22 18:51:39 +0700

    preprared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,29 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware (req la client gui len vd : get; put; pull; delete, res la server tra ve vd nhu json;200;404)
+if(process.env.NODE_ENV !== "production") {
 app.use(cors({
     origin: "http://localhost:5173", // cho phep frontend gui request den backend
 })); 
+}
 app.use(express.json()); // this middleware parses incoming JSON requests and puts the parsed data in req.body 
 app.use(rateLimiter);
 
 
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
+}
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () =>{
